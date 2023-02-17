c53929c9bb4feb6598ddcbfbb8e0e0f56018e290
andyfroberg <andy.froberg@gmail.com>
2023-02-17 13:12:50 -0800

    added GameEntryPoint

--- game_entry_point.py
+++ game_entry_point.py
@@ -3,14 +3,14 @@ from controller_console import ControllerConsole
 from model import Model
 
 
-class Main:
+class GameEntryPoint:
     pass
 
 
 if __name__ == "__main__":
     m = Model()
-    # v = ViewConsole()
-    # c = ControllerConsole(m, v)
+    v = ViewConsole()
+    c = ControllerConsole(m, v)
 
     # m.register_controller(c)
     # v.register_controller(c)

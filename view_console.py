4cc2c84fbc1c43b49c596e63f3d7585e19ac4bf5
fikadulem <fikadulem@gmail.com>
2023-02-20 20:57:49 -0800

    Added methods for view console

--- view_console.py
+++ view_console.py
@@ -8,3 +8,27 @@ class ViewConsole(View):
     console."""
     def __init__(self, controller: Controller) -> None:
         self.__controller: Controller = controller
+        self.__model = controller.model
+        self.__model.add_view(self)
+
+    def display_start_screen(self) -> None:
+        print("Welcome to the Dungeon!")
+
+    def display_current_room(self, room_description: str) -> None:
+        print(room_description)
+
+    def display_player_input_prompt(self) -> None:
+        print("What do you want to do next?")
+
+    def get_player_input(self) -> str:
+        return input()
+
+    def display_player_dead_message(self) -> None:
+        print("You died!")
+
+    def display_player_won_message(self) -> None:
+        print("Congratulations! You won!")
+
+    def display_player_status(self, hp: int, inventory: dict) -> None:
+        print(f"HP: {hp}")
+        print(f"Inventory: {inventory}")

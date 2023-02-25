a82341e1b261bcd648b5562997a6543cbdbfbe85
andyfroberg <andy.froberg@gmail.com>
2023-02-25 10:16:17 -0800

    fixed bug in loading new rooms

--- all_in_one.py
+++ all_in_one.py
@@ -3,7 +3,7 @@ import math
 import sys
 
 
-class Game2D:
+class AllInOne:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(Settings.SCREEN_RESOLUTION)
@@ -55,9 +55,9 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(
             'sprites/warrior_sp_1.png').convert_alpha()
         self.rect = self.image.get_rect(
-            topleft=(position[0] * Settings.PIXEL_SCALE,
+            center=(position[0] * Settings.PIXEL_SCALE,
                      position[1] * Settings.PIXEL_SCALE))
-
+        # self.rect = self.image.get_rect()
 
     def move(self, keys):
         dx, dy = 0, 0
@@ -88,20 +88,20 @@ class Player(pygame.sprite.Sprite):
             self.set_pos_new_room(dx, dy)
 
     def can_move_x(self, dx):
-        return (int(self.x + dx), int(self.y)) not in self.game.dungeon.visible_map
+        return (int(self.x + dx), int(self.y)) not in self.game.dungeon.visible_room
 
     def can_move_y(self, dy):
-        return (int(self.x), int(self.y + dy)) not in self.game.dungeon.visible_map
+        return (int(self.x), int(self.y + dy)) not in self.game.dungeon.visible_room
 
     def pass_through_door(self, dx, dy):
         new_pos = (int(self.x + dx), int(self.y + dy))
 
-        return new_pos in self.game.dungeon.visible_map \
-            and self.game.dungeon.visible_map[new_pos] == 2
+        return new_pos in self.game.dungeon.visible_room \
+            and self.game.dungeon.visible_room[new_pos] == 2
 
     def set_pos_new_room(self, dx, dy):
         # Precondition - already have checked that door_pos is in
-        # dungeon.visible_map (in pass_through_door() function)
+        # dungeon.visible_room (in pass_through_door() function)
         door_pos = (int(self.x + dx), int(self.y + dy))
 
         # heading north
@@ -121,17 +121,13 @@ class Player(pygame.sprite.Sprite):
             self.x = 1
             self.rect.x = 1 * Settings.PIXEL_SCALE
 
-    # def draw(self):
-    #     pass
-        # Tile((self.x, self.y), [self.visible_sprites])
-        # pygame.draw.circle(self.game.screen,
-        #                    'green',
-        #                    (self.x * 100, self.y * 100),
-        #                    15)
-
     def draw(self):
-        pass
+        # Tile((self.x, self.y), [self.visible_sprites])
 
+        pygame.draw.circle(self.game.screen,
+                           'green',
+                           (self.x * Settings.PIXEL_SCALE, self.y * Settings.PIXEL_SCALE),
+                           15)
 
 ### Citation 001 - How to create a Zelda style game in python
 #   https://www.youtube.com/watch?v=cwWi05Icpw0
@@ -141,15 +137,8 @@ class Player(pygame.sprite.Sprite):
 class Tile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('sprites/brick_brown_100x100_v3.png').convert_alpha()
+        self.image = pygame.image.load('sprites/brick_brown_50x50_v3.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
-
-class Player1(pygame.sprite.Sprite):
-    def __init__(self, position, groups):
-        super().__init__(groups)
-        self.image = pygame.image.load('sprites/warrior_sp_1.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=position)
-
 ### End Citation 001
 
 class Dungeon:
@@ -162,45 +151,46 @@ class Dungeon:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
         self.START_MAP = [
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 2],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 1, 1, 2, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ]
-        self.visible_map = {}
+        self.visible_room = {}
         self.load_map(self.START_MAP)
         self.second_room = [
-            [1, 1, 2, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [2, 0, 0, 0, 0, 0, 0, 2],
-            [1, 0, 0, 1, 1, 1, 0, 1],
-            [1, 0, 0, 1, 0, 0, 0, 1],
-            [1, 0, 0, 1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 2, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2],
+            [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
         ]
 
     def load_map(self, room):
-        self.current_room_size = (len(room), len(room[0]))
+        self.current_room_size = (len(room[0]), len(room))
         self.visible_sprites.empty()  # Clear sprites from previous room
-        self.visible_map.clear()
+        self.visible_room.clear()
 
         for i, row in enumerate(room):
             for j, col in enumerate(row):
                 if room[i][j]:
-                    self.visible_map[(j, i)] = room[i][j]
-                    if self.visible_map[(j, i)] == 1:
+                    self.visible_room[(j, i)] = room[i][j]
+                    if self.visible_room[(j, i)] == 1:
                         Tile((j * Settings.PIXEL_SCALE, i * Settings.PIXEL_SCALE), [self.visible_sprites])
 
     def draw(self):
         self.visible_sprites.draw(self.surface)
-        self.player_sprites.draw(self.surface)
+        # self.player_sprites.draw(self.surface)
+        # self.game.screen.blit(self.game.player.image, self.game.player.rect)
         self.visible_sprites.update()
-        # for location in self.visible_map:
+        # for location in self.visible_room:
         #     pygame.draw.rect(self.game.screen,                                  # Screen to write to
         #                      'white',                                           # Color
         #                      (location[1] * 100, location[0] * 100, 100, 100),  # Rect size
@@ -216,10 +206,10 @@ class Settings:
     PLAYER_SPEED = 0.05
     PLAYER_ROTATION_SPEED = 0.05
 
-    SCREEN_RESOLUTION = (1000, 800)  # (width, height)
+    SCREEN_RESOLUTION = (700, 500)  # (width, height)
     FPS = 60  # Frames per second
 
 
 if __name__ == "__main__":
-    game = Game2D()
+    game = AllInOne()
     game.loop()

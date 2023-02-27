ed2d1d28bce5e096059d6acc849a026cef9493a0
andyfroberg <andy.froberg@gmail.com>
2023-02-26 19:28:50 -0800

    added floor, rock, and door sprites

--- sprite_door.py
+++ sprite_door.py
@@ -5,9 +5,9 @@ import pygame
 #
 # The sprite functionality of this game was heavily influenced by the
 # above video which explains how to use sprites in Pygame.
-class Tile(pygame.sprite.Sprite):
+class SpriteDoor(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('sprites/brick_brown_50x50_v3.png').convert_alpha()
+        self.image = pygame.image.load('sprites/door_50x50_v2.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
 ### End Citation 001
--- view_2d.py
+++ view_2d.py
@@ -1,16 +1,14 @@
 import pygame
 from settings import Settings
-from tile import Tile
+from sprite_floor import SpriteFloor
+from sprite_brick import SpriteBrick
+from sprite_door import SpriteDoor
+from sprite_rock import SpriteRock
 from player_sprite import PlayerSprite
 
 class View2D:
     def __init__(self, model):
         model.register_view(self)
-
-        # Technically don't need a ref to cont. because we call
-        # for event in pygame.event.get():
-        # and pygame.keys.get_pressed() in the controller
-        # self.__controller = controller
 
         self.screen = pygame.display.set_mode(Settings.SCREEN_RESOLUTION)
         self.surface = pygame.display.get_surface()
@@ -33,16 +31,14 @@ class View2D:
         self.draw_frame(dungeon, player)
 
     def draw_frame(self, dungeon, player):
-        self.screen.fill((0, 0, 0))
+        pygame.display.set_caption('Dungeon Adventure 2.0')
+        self.screen.fill((178, 178, 178))
         # draw dungeon
         self.draw_dungeon(dungeon) # can we pass only a subset of model state for only needed parts for dungeon?
         # draw player
         self.draw_player(player)# can we pass only a subset of model state for only needed parts for player?
         # draw player HUD
         self.draw_hud(player)
-
-        # Extras
-        pygame.display.set_caption('Dungeon Adventure 2.0')
 
         # Call update
         pygame.display.update()
@@ -57,46 +53,22 @@ class View2D:
         self.visible_sprites.empty()  # Clear sprites from previous room
         self.room_ui.clear()  # Need to move - view should not change state
 
-        # for i, row in enumerate(dungeon.current_room):
-        #     for j, col in enumerate(row):
-        #         if dungeon.current_room[i][j]:
-        #             self.room_ui[(j, i)] = dungeon.current_room[i][j]
-        #             if self.room_ui[(j, i)] == 1:
-        #                 Tile((j * Settings.PIXEL_SCALE,
-        #                       i * Settings.PIXEL_SCALE),
-        #                      [self.visible_sprites])
-
-        # for i, row in enumerate(dungeon.current_room):
-        #     for j, col in enumerate(row):
-        #         self.room_ui[(j, i)] = dungeon.current_room[(j, i)]
-        #         if self.room_ui[(i, j)] == 1:
-        #             Tile((j * Settings.PIXEL_SCALE,
-        #                   i * Settings.PIXEL_SCALE),
-        #                  [self.visible_sprites])
-
         self.room_ui = dungeon.current_room.copy()
         for row, col in self.room_ui.keys():
+            if self.room_ui[(row, col)] == 0:
+                SpriteFloor((row * Settings.PIXEL_SCALE, col * Settings.PIXEL_SCALE),
+                     [self.visible_sprites])
             if self.room_ui[(row, col)] == 1:
-                Tile((row * Settings.PIXEL_SCALE, col * Settings.PIXEL_SCALE),
+                SpriteBrick((row * Settings.PIXEL_SCALE, col * Settings.PIXEL_SCALE),
+                     [self.visible_sprites])
+            elif self.room_ui[(row, col)] == 2:
+                SpriteDoor((row * Settings.PIXEL_SCALE, col * Settings.PIXEL_SCALE),
+                     [self.visible_sprites])
+            elif self.room_ui[(row, col)] == 3:
+                SpriteRock((row * Settings.PIXEL_SCALE, col * Settings.PIXEL_SCALE),
                      [self.visible_sprites])
 
         self.visible_sprites.draw(self.surface)
-
-
-        # Load room
-        # size = (len(room[0]), len(room))
-        # self.visible_sprites.empty()  # Clear sprites from previous room
-        # self.visible_room.clear()
-        #
-        # for i, row in enumerate(room):
-        #     for j, col in enumerate(row):
-        #         if room[i][j]:
-        #             self.visible_room[(j, i)] = room[i][j]
-        #             if self.visible_room[(j, i)] == 1:
-        #                 Tile((j * Settings.PIXEL_SCALE,
-        #                       i * Settings.PIXEL_SCALE),
-        #                      [self.visible_sprites])
-
 
     def draw_player(self, player):
         if not self.player_sprite:
@@ -111,11 +83,10 @@ class View2D:
     def draw_hud(self, player):
         pygame.draw.rect(self.screen, (20, 95, 95), (0, 400, 700, 100))
 
-
-
         ##### There appears to be a known issue with Pygame fonts
         ##### on M1 Macs. Can work on fixing this later. (Maybe
         ##### just use sprites to spell out the HUD info?)
+        ##### https://github.com/pygame/pygame/issues/2500
         # my_font = pygame.font.Font('fonts/Mitr-Medium.ttf', 30)
         #
         # img = my_font.render('Some Text', True, (0, 0, 0))
